287298fdecfbbdb4900eb355a51b3752ca3de01c
Lucas da Paz <xlucaspx@gmail.com>
2023-04-13 16:39:32 -0300

    utilizando cors

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const routes = require("../routes");
 
 const app = express();
+app.use(cors());
+
 const port = 3000;
 routes(app);
 
