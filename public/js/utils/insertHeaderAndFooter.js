c90d345ee3897d77a359e122ffb016141a262078
Lucas da Paz <xlucaspx@gmail.com>
2023-05-03 11:42:03 -0300

    alterando identidade visual do projeto
    adequando projeto às orientaçÃões do comitê de sustentabilidade

--- public/js/utils/insertHeaderAndFooter.js
+++ public/js/utils/insertHeaderAndFooter.js
@@ -1,10 +1,4 @@
 export function insertHeaderAndFooter() {
-  const logo = `<div class="logo">
-      <a href="index.html" class="logo__link" title="Home | Programa Somar">
-        <img src="../img/logos/logo_cores_ods.svg" alt width="50" height="50">
-        <h1 class="logo__texto">Programa Somar</h1>
-      </a>
-    </div>`;
   const nav = `<nav class="nav__menu">
         <ul class="nav__list">
           <li class="nav__list__item">
@@ -12,9 +6,6 @@ export function insertHeaderAndFooter() {
           </li>
           <li class="nav__list__item">
             <a href="index.html#ods" class="nav__link">ODS</a>
-          </li>
-          <li class="nav__list__item">
-            <a href="index.html#contato" class="nav__link">Contato</a>
           </li>
           <li class="nav__list__item">
             <a href="projetos.html" class="nav__link">Projetos</a>
@@ -25,26 +16,37 @@ export function insertHeaderAndFooter() {
   const header = document.getElementById("dynamic_header");
   const footer = document.getElementById("dynamic_footer");
 
-  header.innerHTML = `${logo}
+  header.innerHTML = `
+    <a href="index.html" class="logo__link" title="Home | Programa Somar">
+      <img src="../img/logos/logo_programa_somar.png" alt width="260" height="60">
+    </a>
 
     <div class="header__div">
       ${nav}
 
       <button type="button" id="btnLog" class="btnLog btn btnSecundario"></button>
-    </div>`;
+    </div>
+  `;
 
-  footer.innerHTML = `${logo}
-  
-      ${nav}
-  
+  footer.innerHTML = `
+    <div class="footer__div">
+      <img src="../img/logos/logo_fecomercio_branco.png" alt class="footer__img"></a>
+    
       <ul class="footer__social">
         <li><a href="https://www.linkedin.com/school/senac-rs/" class="social__link social__link--linkedin"
-            target="_blank" title="Senac RS | LinkedIn"></a></li>
+            target="_blank" title="Senac RS | LinkedIn"></a>
+        </li>
         <li><a href="https://www.instagram.com/senactech/" class="social__link social__link--instagram" target="_blank"
-            title="Senac Tech | Instagram"></a></li>
+            title="Senac Tech | Instagram"></a>
+        </li>
         <li><a href="https://twitter.com/senacrs" class="social__link social__link--twitter" target="_blank"
-            title="Senac RS | Twitter"></a></li>
+            title="Senac RS | Twitter"></a>
+        </li>
       </ul>
 
-      <p class="creditos">Devs da 222 &copy; 2023</p>`;
+      ${nav}
+    </div>
+
+    <p class="creditos">Devs da 222 &copy; 2023</p>
+  `;
 }
