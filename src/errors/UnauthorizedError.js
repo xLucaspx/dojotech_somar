71db54de0cee43f8479beae6c171b5827441d75e
Lucas da Paz Oliveira <xlucaspx@gmail.com>
2024-06-19 11:18:30 -0300

    mantendo apenas front-end e utilizando api em php

--- src/errors/UnauthorizedError.js
+++
@@ -1,8 +0,0 @@
-class UnauthorizedError extends Error {
-  constructor(message, status = 401) {
-    super(message);
-    this.status = status;
-  }
-}
-
-module.exports = UnauthorizedError;
--- src/utils/token/geraJwt.js
+++
@@ -1,8 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-function geraJwt(payload) {
-  // Informações sensíveis como o segredo do token são armazenadas em variáveis de ambiente
-  return jwt.sign(payload, process.env.TOKEN_SECRET, { expiresIn: "6h" });
-}
-
-module.exports = geraJwt;
